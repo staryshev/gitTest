f6d2d1bdfa9cc11069d85be26adca3c605fac854
Roman Staryshev <staryshe@cs.petrsu.ru>
2020-10-06 12:20:21 +0300

    add at and makedir

---
+++ at/src/fun2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "head.h"
+
+void function2()
+{
+    printf("3\n");
+}
---
+++ makedir/src/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "head.h"
+
+int main()
+{
+    /* Privet */
+    printf("1");
+    function1();
+    function2();
+    return 0;
+}
